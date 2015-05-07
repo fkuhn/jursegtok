7c31cb011f45dade5c9b5e5a2f00cb7f4edbf082
Florian Kuhn <fkuhn@posteo.de>
2015-05-08 01:26:15 +0200

    first tokenizer version

--- src/sentence/tokenizer.py
+++ src/sentence/tokenizer.py
@@ -1,1 +1,22 @@
 __author__ = 'kuhn'
+
+import nltk
+import codecs
+
+
+class JurSentTokenize(object):
+
+    def _init__(self):
+
+        self.abbreviations = codecs.open('../../data/legal_abbrv.txt', encoding='utf-8').readlines()
+
+        self.sent_tokenize = nltk.data.load('tokenizers/punkt/german.pickle')
+        self.sent_tokenize._params.abbrev_types.update(set(self.abbreviations))
+
+    def sentence_tokenize(self, data):
+
+        sent = self.sent_tokenize.tokenize(data)
+
+        return sent
+
+
