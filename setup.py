334f75fa6a7ce9f40b1b22671c6a42a4c3592f9c
Florian Kuhn <fkuhn@posteo.de>
2015-05-09 10:31:36 +0200

    installed base setup

---
+++ setup.py
@@ -0,0 +1,40 @@
+__author__ = 'kuhn'
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'lxml', 'scikit-learn', 'nltk', 'segtok'
+    ]
+
+
+setup(name='jursegtok',
+    version=version,
+    description="legal domain specified tokenization",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='tokenization batch python legal',
+    author='Florian Kuhn',
+    author_email='kuhn@ids-mannheim.de',
+    url='www.ids-mannheim.de',
+    license='Apache v2',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['jursegtok=jursegtok:main']
+    }
+)
