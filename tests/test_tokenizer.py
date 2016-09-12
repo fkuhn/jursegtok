e92aba0be87433be04cfb5498eea37bb834cfac4
Florian Kuhn <kuhn@ids-mannheim.de>
2016-09-12 12:33:21 +0200

    added files for later testing

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,36 @@
+import os
+import pytest
+import types
+import nltk
+from jursegtok import tokenizer, corpus
+
+TESTDOC = corpus.OJDocument('896152.html')
+
+
+"""
+This module contains tests for the jursegtok tokenizer
+"""
+
+TESTFILE = '896152.html'
+
+
+class TestTokenizer(object):
+    """
+    initialize the tokenizer,
+    sentence tokenize example docs and test the expected outcome.
+    """
+
+    def setup(self):
+
+        testdoc = corpus.OJDocument(TESTFILE)
+        plain = testdoc.plain_text()
+        assert type(plain, types.StringType)
+        assert len(plain) > 0
+
+        tok = tokenizer.JurSentTokenizer()
+        assert type(tok.get_tokenizer_model(), nltk.tokenize.punkt.PunktSentenceTokenizer)
+
+    def test_abbreviations(self):
+        pass
+    def test_tokenizing(self):
+        pass
