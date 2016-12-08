96074b815903f37e905eb3556fb2d405e24f1824
Florian Kuhn <kuhn@ids-mannheim.de>
2016-12-08 12:11:17 +0100

    added cli module

---
+++ src/jursegtok/cli.py
@@ -0,0 +1,20 @@
+import os
+import argparse
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("intext")
+
+# PARSER.add_subparsers()
+
+
+def main():
+    pass
+
+    arguments = PARSER.parse_args()
+    print arguments.intext
+
+
+
+def prepare_tokenizer():
+    pass
+
