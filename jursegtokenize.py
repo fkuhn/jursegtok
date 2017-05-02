502456f6cc4c8293fe6268d0ac119ae74362c2cb
kuhn <fkuhn@posteo.de>
2017-05-02 23:54:35 +0200

    console application

--- jursegtokenize.py
+++ jursegtokenize.py
@@ -4,20 +4,38 @@ import codecs
 from jursegtok import cli, tokenizer, corpus
 
 
-indata = sys.argv[1]
-outdata = sys.argv[2]
+try:
+    indata = sys.argv[1]
+    outdata = sys.argv[2]
+
+except IndexError as e:
+    print "one or more missing parameters"
+
+
 js = tokenizer.JurSentTokenizer()
 
+
 def main():
-
+    """
+    processes the command line arguments and 
+    produces an output
+    :return: 
+    """
     if os.path.isfile(indata):
-        with corpus.OJDocument(indata) as textdata:
-            output = js.sentence_tokenize(textdata)
-
+        textdata = corpus.OJDocument(indata)
+        output = js.sentence_tokenize(textdata.plain_text())
+        for item in output:
+            print item
+            print
+            print "***********************************************"
+            print
         with codecs.open(outdata, mode='w', encoding='utf-8') as outfile:
 
             for sentence in output:
-                outfile.writeline(sentence)
+                outfile.write(sentence+'\n')
 
 if __name__ == "__main__":
     main()
+
+
+
