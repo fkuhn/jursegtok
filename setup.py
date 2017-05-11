857dfcf718b08b5b4aae75fb2ed21fe16bb7f948
kuhn <fkuhn@posteo.de>
2017-05-11 20:32:28 +0200

    removed unused requirements

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ README = open(os.path.join(here, 'README.md')).read()
 NEWS = open(os.path.join(here, 'NEWS.md')).read()
 
 
-version = '0.1'
+version = '0.2'
 
 install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-    'lxml', 'scikit-learn', 'nltk', 'segtok', 'hickle'
+    'lxml', 'scikit-learn', 'nltk', 'hickle'
     ]
-
 
 setup(name='jursegtok',
     version=version,
@@ -40,3 +39,4 @@ setup(name='jursegtok',
             ['jursegtok=jursegtok.cli:main']
     }
 )
+
